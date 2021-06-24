1a4f074f7f8460966efc7163a1d5db88f4d1e91a
John Ross <johnyboy11@hotmail.co.uk>
2021-06-24 10:45:45 +0100

    "Unannotated course class created"

--- src/main/java/com/example/codeclan/CourseBooking/models/Course.java
+++ src/main/java/com/example/codeclan/CourseBooking/models/Course.java
@@ -1,4 +1,50 @@
 package com.example.codeclan.CourseBooking.models;
 
 public class Course {
+
+    private Long id;
+
+    private String name;
+
+    private String town;
+
+    private Integer rating;
+
+    public Course(String name, String town, Integer rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
 }
