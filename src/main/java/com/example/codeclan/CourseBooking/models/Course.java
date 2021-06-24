bf9c8c19d6109146de124b672b87906046710922
John Ross <johnyboy11@hotmail.co.uk>
2021-06-24 11:05:50 +0100

    "Course annotated"

--- src/main/java/com/example/codeclan/CourseBooking/models/Course.java
+++ src/main/java/com/example/codeclan/CourseBooking/models/Course.java
@@ -1,26 +1,45 @@
 package com.example.codeclan.CourseBooking.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "courses")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "town")
     private String town;
 
+    @Column(name = "rating")
     private Integer rating;
 
-    private ArrayList<Booking> bookings;
+    @JsonBackReference
+    @OneToMany(mappedBy = "course")
+    private List<Booking> bookings;
 
     public Course(String name, String town, Integer rating) {
         this.name = name;
         this.town = town;
         this.rating = rating;
         this.bookings = new ArrayList<>();
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
     }
 
     public Long getId() {
