df72d34d3e34be123eb60ebb0d4528a88e69c637
John Ross <johnyboy11@hotmail.co.uk>
2021-06-24 10:48:24 +0100

    "Unannotated booking class created"

--- src/main/java/com/example/codeclan/CourseBooking/models/Booking.java
+++ src/main/java/com/example/codeclan/CourseBooking/models/Booking.java
@@ -2,6 +2,41 @@ package com.example.codeclan.CourseBooking.models;
 
 public class Booking {
 
+    private Long id;
 
+    private String date;
 
+    private Booking booking;
+
+    private Customer customer;
+
+    public Booking(String date, Booking booking, Customer customer) {
+        this.date = date;
+        this.booking = booking;
+        this.customer = customer;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
